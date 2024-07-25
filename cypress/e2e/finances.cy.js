9a563b30a6e9c6065ee9271a4ac24cb706be1c2a
Tiago Paulo <api_whatsapp@hotmail.com>
2024-07-25 02:19:58 -0300

    criado delete para transações criadas

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,16 +1,25 @@
 describe("Transações", () => {
   beforeEach(() => {
-    cy.visit("https://devfinance-agilizei.netlify.app/#");
+    cy.visit("https://devfinance-agilizei.netlify.app/");
   });
 
   it("Cadastrar uma entrada", () => {
     criarTransacao("Salário", 2800);
-
-    // cy.get("tbody tr td.description").should("have.text", "Freela")
   });
 
   it("Cadastrar uma saída", () => {
     criarTransacao("Cinema", -50);
+  });
+
+  it("Excluir transação", () => {
+    criarTransacao("Conta de luz", -110);
+    criarTransacao("Cartão de crédito", 473);
+    criarTransacao("Gorjeta", -20);
+
+    cy.contains(".description", "Conta de luz")
+    .parent()
+    .find("img")
+    .click();
   });
 });
 
@@ -23,5 +32,5 @@ function criarTransacao(description, amount) {
 
   cy.contains("button", "Salvar").click();
 
-  cy.get("tbody tr td.description").should("have.text", description);
+  cy.contains(".description", description).should("have.text", description)
 }
